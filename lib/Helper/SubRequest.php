beaafc09674d581e63cf921ff04e92edf310dd78
Andy Truong <thehongtt@gmail.com>
2013-11-14 23:11:26 +0700

    + New helper: Sub request.

---
+++ lib/Helper/SubRequest.php
@@ -0,0 +1,20 @@
+<?php
+namespace Drupal\at_base\Helper;
+
+class SubRequest {
+  private $path;
+  private $original_path;
+
+  public function __construct($path) {
+    $this->path = $path;
+    $this->original_path = $_GET['q'];
+  }
+
+  public function __destruct() {
+    $_GET['q'] = $this->original_path;
+  }
+
+  public function request() {
+    return menu_execute_active_handler($this->path, $deliver = FALSE);
+  }
+}
