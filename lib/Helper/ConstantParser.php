a6fc6b6136a36314f71c8acc604a8a0b0300c1e5
Andy Truong <thehongtt@gmail.com>
2013-11-14 22:11:51 +0700

    + New helper: Constant Parser.

---
+++ lib/Helper/ConstantParser.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\at_base\Helper;
+
+/**
+ * Parse 'CONSTANT', 'CONSTANT_1 | CONSTANT_2', 'CONSTANT_1 & CONSTANT_2' to
+ * real value.
+ */
+class ConstantParser {
+  private $string;
+
+  public function __construct($string) {
+    $this->string = $string;
+  }
+
+  public function parse() {
+    if (preg_match('/^[A-Z_]+$/', $this->string)) {
+      return constant($this->string);
+    }
+
+    return $this->parseOperator();
+  }
+
+  private function parseOperator() {
+    $part = preg_split('/\s+[\|&]\s+/', $this->string);
+    if (count($part)) {
+      foreach ($part as $i => $constant) {
+        $part[$i] = constant($constant);
+      }
+
+      return strpos($this->string, '|') ? ($part[0] | $part[1]) : $part[0] & $part[1];
+    }
+  }
+}
