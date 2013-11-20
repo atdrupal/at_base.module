c4ce21aeb57edd365e327e6e97d6744aaad97f2d
Andy Truong <thehongtt@gmail.com>
2013-11-20 09:30:51 +0700

    + Class to find real path for path that has tokens.

---
+++ lib/Helper/RealPath.php
@@ -0,0 +1,34 @@
+<?php
+namespace Drupal\at_base\Helper;
+
+/**
+ * Tool to replace tokens with real values:
+ *
+ *  %theme => /path/to/active_theme/
+ *  @at_base => /path/to/at_base/
+ */
+class RealPath {
+  private $path;
+
+  public function __construct($path) {
+    $this->path = $path;
+  }
+
+  public function get() {
+    $real_path = $this->path;
+
+    if (strpos($real_path, '%theme/') !== FALSE) {
+      $real_path = str_replace('%theme/', path_to_theme() . '/templates/', $real_path);
+    }
+
+    preg_match('/@([a-z_]+)/', $real_path, $matches);
+    if (!empty($matches)) {
+      $module = $matches[1];
+      if ($module_path = drupal_get_path('module', $module)) {
+        $real_path = str_replace("@{$module}/", $module_path . '/templates/', $real_path);
+      }
+    }
+
+    return $real_path;
+  }
+}
