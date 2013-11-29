0313b7e2c115d5ede1b1596d6acdc57d6e5a7199
Andy Truong <thehongtt@gmail.com>
2013-11-29 14:52:10 +0700

    #8: WarmerConfig does not know event name.

--- lib/Cache/WarmerConfig.php
+++ lib/Cache/WarmerConfig.php
@@ -2,6 +2,12 @@
 namespace Drupal\at_base\Cache;
 
 class WarmerConfig {
+  private $event_name;
+
+  public function __construct($event_name) {
+    $this->event_name = $event_name;
+  }
+
   private function getModules() {
     foreach (at_modules('at_base') as $module) {
       $file = DRUPAL_ROOT . '/' . drupal_get_path('module', $module) . '/config/cache_warming.yml';
