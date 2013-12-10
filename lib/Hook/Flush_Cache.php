95d29ec2550190ab61ca6f5acd5e8067f8e50577
Andy Truong <thehongtt@gmail.com>
2013-12-10 15:03:18 +0700

    Fix #1: Auto change module weight if value is defined in %module.info > Also update module weight on module enabled.

--- lib/Hook/Flush_Cache.php
+++ lib/Hook/Flush_Cache.php
@@ -32,14 +32,6 @@ class Flush_Cache {
   }
 
   private function fixModuleWeight() {
-    foreach (system_list('module_enabled') as $module_name => $project) {
-      if (!empty($project->info['weight'])) {
-        $weight = $project->info['weight'];
-        if (is_numeric($weight)) {
-          $sql = "UPDATE {system} SET weight = :weight WHERE name = :name";
-          db_query($sql, array(':weight' => $weight, ':name' => $module_name));
-        }
-      }
-    }
+    at_id(new \Drupal\at_base\Helper\Module_Weight())->execute();
   }
 }
