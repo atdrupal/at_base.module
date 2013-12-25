00b14f884cef9de8352dfedccc77c65d13468b66
Andy Truong <thehongtt@gmail.com>
2013-12-25 16:39:55 +0700

    ‘Module weight fixer’ is not a class. Remove it.

--- lib/Hook/Flush_Cache.php
+++ lib/Hook/Flush_Cache.php
@@ -26,7 +26,18 @@ class Flush_Cache {
     at_modules('at_base', TRUE);
   }
 
-  private function fixModuleWeight() {
-    at_id(new \Drupal\at_base\Helper\Module_Weight())->execute();
+  /**
+   * Update module's weight value in system table.
+   */
+  public function fixModuleWeight() {
+    foreach (system_list('module_enabled') as $module_name => $project) {
+      if (!empty($project->info['weight'])) {
+        $weight = $project->info['weight'];
+        if (is_numeric($weight)) {
+          $sql = "UPDATE {system} SET weight = :weight WHERE name = :name";
+          db_query($sql, array(':weight' => $weight, ':name' => $module_name));
+        }
+      }
+    }
   }
 }
