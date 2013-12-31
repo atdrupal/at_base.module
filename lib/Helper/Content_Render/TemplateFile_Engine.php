c0dca7ff0bbd77e4fcefec748af199ff30040248
Andy Truong <andy@toila.net>
2013-12-31 15:53:02 +0700

    Update TemplateFile_Engine.php
    Avoid fatal error.

--- lib/Helper/Content_Render/TemplateFile_Engine.php
+++ lib/Helper/Content_Render/TemplateFile_Engine.php
@@ -6,7 +6,7 @@ class TemplateFile_Engine extends String_Engine {
   public function process() {
     return \AT::twig()->render(
       at_container('helper.real_path')->get($this->data['template']),
-      $this->data['variables']
+      $this->data['variables'] ? $this->data['variables'] : array()
     );
   }
 }
