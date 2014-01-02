c06c08e90c64f8f689fb0074fb04ce87df6f3cb3
Tien Vo Xuan <tien@go1.com.au>
2014-01-02 14:29:58 +0700

    Find service by tag.

--- lib/Container/Definition.php
+++ lib/Container/Definition.php
@@ -28,4 +28,50 @@ class Definition {
       return $services;
     });
   }
+
+  /**
+   * Returns service's definitions for a given tag.
+   *
+   * @param string $name The tag name
+   *
+   * @return array An array of tagged service's definitions.
+   *
+   * @api
+   */
+  public static function findByTag($tag) {
+    $options = array('ttl' => '+ 1 year', 'cache_id' => 'at_base:tagged_services:{$tag}');
+
+    return at_cache($options, function() {
+      $services = array();
+      $all_services = Definition::getAll();
+      foreach ($all_services as $service_name => $service) {
+        if (!isset($service['tags'])) {
+          continue;
+        }
+        if (in_array($tag, $service['tags'])) {
+          $services[$service_name] = $service;
+        }
+      }
+
+      return $services;
+    });
+  }
+
+  /**
+   * Returns all tags the defined services use.
+   *
+   * @return array An array of tags
+   */
+  public static function findTags() {
+    $options = array('ttl' => '+ 1 year', 'cache_id' => 'at_base:service_tags');
+
+    return at_cache($options, function() {
+      $tags = array();
+      foreach (Definition::getAll() as $service_name => $service) {
+        $tags = array_merge($service['tags'], $tags);
+      }
+
+      return array_unique($tags);
+    });
+  }
 }
