bca6631475a41305ee9507a901bbeaa6febab7d1
Andy Truong <thehongtt@gmail.com>
2014-01-03 15:35:06 +0700

    Fix broken test case.

--- lib/Helper/Content_Render/Callback_Engine.php
+++ lib/Helper/Content_Render/Callback_Engine.php
@@ -4,6 +4,6 @@ namespace Drupal\at_base\Helper\Content_Render;
 
 class Callback_Engine extends String_Engine {
   public function process() {
-    return call_user_func_array($this->data['callback'], $this->data['arguments']);
+    return call_user_func_array($this->data['callback'], $this->data['variables'] ? $this->data['variables'] : array());
   }
 }
