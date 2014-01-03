74e4f4b7f086b0773b0046cd5be6c12c54965995
Andy Truong <andy@toila.net>
2014-01-03 15:00:58 +0700

    Create Callback_Engine.php

---
+++ lib/Helper/Content_Render/Callback_Engine.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Drupal\at_base\Helper\Content_Render;
+
+class Callback_Engine extends String_Engine {
+  public function process() {
+    return call_user_func_array($this->data['callback'], $this->data['arguments']);
+  }
+}
