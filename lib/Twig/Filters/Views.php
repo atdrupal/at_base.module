1594e6a70ae4d0ea77a3de78d96c9acc3884fcd3
Andy Truong <thehongtt@gmail.com>
2014-01-07 15:19:39 +0700

    Fix #86: Twig Filter > drupalView > Support template.

--- lib/Twig/Filters/Views.php
+++ lib/Twig/Filters/Views.php
@@ -2,44 +2,76 @@
 
 namespace Drupal\at_base\Twig\Filters;
 
+/**
+ * Callback for drupalView Twig filter.
+ *
+ * @todo  More test case for view with custom template, …
+ */
 class Views {
+  private $view;
+  private $name;
+  private $display_id = 'default';
+  private $template;
+  private $arguments = array();
+
+  public function __construct($name, $display_id = 'default', $arguments = array()) {
+    if (!$this->view = views_get_view($name)) {
+      throw new \Exception('View not found: '. $this->name);
+    }
+
+    $this->name = $name;
+    $this->setDisplayId($display_id);
+    $this->setArguments($arguments);
+  }
+
+  public function setDisplayId($display_id) {
+    $this->display_id = $display_id;
+    $this->view->set_display($display_id);
+
+    if (!$this->view->access($this->display_id)) {
+      throw new \Exception('<!-- Access denied: '. $this->name .':'. $this->display_id .' -->');
+    }
+  }
+
+  public function setArguments($arguments) {
+    $this->arguments = $arguments;
+
+    if (is_array($this->arguments)) {
+      $this->view->set_arguments($this->arguments);
+    }
+  }
+
+  public function setTemplate($template) {
+    $this->template = at_container('helper.real_path')->get($template);
+  }
+
   /**
-   * Callback for drupalView filter.
+   * Find Twig template for view on context theme.
    *
-   * @param  string $name       Name of view
-   * @param  string $display_id Display ID of view
-   * @return string
+   * @todo Remove this magic
    */
-  public static function render($name, $display_id = 'default') {
-    $args = func_get_args();
-    array_shift($args); // remove $name
-    if (count($args)) {
-      array_shift($args); // remove $display_id
+  protected function suggestTemplate() {
+    $suggestions[] = path_to_theme() . "/templates/views/{$this->name}.{$this->display_id}.html.twig";
+    $suggestions[] = path_to_theme() . "/templates/views/{$this->name}.html.twig";
+    foreach ($suggestions as $path) {
+      if (is_file(DRUPAL_ROOT . '/' . $path)) {
+        return $path;
+      }
+    }
+  }
+
+  public function execute() {
+    // ---------------------
+    // No template, use default
+    // ---------------------
+    if (!$this->template && (!$this->template = $this->suggestTemplate())) {
+      $this->view->pre_execute();
+      return $this->view->preview($this->display_id, $this->arguments);
     }
 
-    if (!function_exists('views_get_view')) {
-      throw new \Exception('Missing module: views');
-    }
-
-    if (!$view = views_get_view($name)) {
-      return '<!-- Views not found -->';
-    }
-
-    if (!$view->access($display_id)) {
-      return '<!-- Access denied -->';
-    }
-
-    $view->set_display($display_id);
-
-    if (is_array($args)) {
-      $view->set_arguments($args);
-    }
-
-    if (!$template_file = self::findTemplate($name, $display_id)) {
-      $view->pre_execute();
-      return $view->preview($display_id, $args);
-    }
-
+    // ---------------------
+    // With template
+    // ---------------------
     // Many tags rendered by views, we get rid of them
     if (!empty($view->display[$display_id]->display_options['fields'])) {
       foreach (array_keys($view->display[$display_id]->display_options['fields']) as $k) {
@@ -48,29 +80,39 @@ class Views {
       }
     }
 
-    $view->pre_execute();
-    $view->execute();
+    $this->view->pre_execute();
+    $this->view->execute();
 
     module_load_include('inc', 'views', 'theme/theme');
-    $vars['view'] = $view;
+    $vars['view'] = $this->view;
     template_preprocess_views_view($vars);
-    return \AT::twig()->render($template_file, $vars);
+    return at_container('twig')->render($this->template, $vars);
   }
 
-  /**
-   * Find Twig template for view on context theme.
-   *
-   * @param  string $name       Name of view
-   * @param  string $display_id Display ID of view
-   * @return string             Path to template file
-   */
-  protected static function findTemplate($name, $display_id) {
-    $suggestions[] = path_to_theme() . "/templates/views/{$name}.{$display_id}.html.twig";
-    $suggestions[] = path_to_theme() . "/templates/views/{$name}.html.twig";
-    foreach ($suggestions as $path) {
-      if (is_file(DRUPAL_ROOT . '/' . $path)) {
-        return $path;
+  public static function render($name, $display_id = 'default') {
+    $args = func_get_args();
+    array_shift($args); // $name
+    if (count($args)) {
+      $a1 = array_shift($args); // $display_id
+    }
+
+    try {
+      $me = new self($name, $display_id, $args);
+
+      if (is_array($a1)) {
+        foreach ($a1 as $k => $v) {
+          switch ($k) {
+            case 'template':   $me->setTemplate($v);  break;
+            case 'display_id': $me->setDisplayId($v); break;
+            case 'arguments':  $me->setArguments($v); break;
+          }
+        }
       }
+
+      return $me->execute();
+    }
+    catch (\Exception $e) {
+      return $e->getMessage();
     }
   }
 }
