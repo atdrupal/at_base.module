d1c1b22a949cdc52b124bfee73e05e32f366f33c
Andy Truong <thehongtt@gmail.com>
2014-01-09 15:06:05 +0700

    + New service: cache.tag_discover

---
+++ lib/Cache/Warming/Tag_Discover.php
@@ -0,0 +1,22 @@
+<?php
+namespace Drupal\at_base\Cache\Warming;
+
+class Tag_Discover {
+  private $event_name;
+
+  public function setEventName($event_name) {
+    $this->event_name = $event_name;
+  }
+
+  public function tags() {
+    foreach (at_modules('at_base', 'cache_warming') as $module) {
+      if ($data = at_config($module, 'cache_warming')->get('tags')) {
+        if (isset($data[$this->event_name])) {
+          return $data[$this->event_name];
+        }
+      }
+    }
+
+    return array();
+  }
+}
