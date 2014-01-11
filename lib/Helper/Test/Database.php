8a6aee639156c14894d372bb14e05a3a0ac9fb1b
Andy Truong <thehongtt@gmail.com>
2014-01-11 16:16:55 +0700

    Test wrapper.database: Fake it.

--- lib/Helper/Test/Database.php
+++ lib/Helper/Test/Database.php
@@ -2,19 +2,23 @@
 namespace Drupal\at_base\Helper\Test;
 
 class Database {
+  public function __call(string $name , array $arguments) {
+    return $this;
+  }
+
   public function select($table, $alias = NULL, array $options = array()) {
-    return db_select($table, $alias, $options);
+    return $this; # db_select($table, $alias, $options);
   }
 
   public function update($table, array $options = array()) {
-    return db_update($table, $options);
+    return $this; // db_update($table, $options);
   }
 
   public function delete($table, array $options = array()) {
-    return db_delete($table, $options);
+    return $this; # db_delete($table, $options);
   }
 
   public function insert($table, array $options = array()) {
-    return db_insert($table, $options);
+    return $this; # db_insert($table, $options);
   }
 }
