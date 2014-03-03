425c9ae6deb20bf8ddd2d53a2e83a24e340b30ad
Andy Truong <andy@toila.net>
2014-03-04 01:10:30 +0700

    Move blocks, attached, cache to separated textareas.

--- at_ui/misc/js/route.edit.js
+++ at_ui/misc/js/route.edit.js
@@ -2,18 +2,25 @@
 
 setTimeout(function(){
 
-var save = function() {
-  $('#edit-code').parents('form').trigger('submit');
-};
+  var save = function() {
+    $('#edit-code').parents('form').trigger('submit');
+  };
 
-var editor = CodeMirror.fromTextArea(document.getElementById("edit-code"), {
-  lineNumbers: true
-  , matchBrackets: true
-  , viewportMargin: Infinity
-  , theme: 'monokai'
-  , extraKeys: {"Cmd-S": save , "Ctrl-S": save}
-  , mode: "yaml"
-});
+  var ids = ['edit-route', 'edit-blocks', 'edit-attached', 'edit-cache'];
+
+  for (var i in ids) {
+    var e = document.getElementById(ids[i]);
+    if (e) {
+      var editor = CodeMirror.fromTextArea(e, {
+        lineNumbers: true
+        , matchBrackets: true
+        , viewportMargin: Infinity
+        , theme: 'monokai'
+        , extraKeys: {"Cmd-S": save , "Ctrl-S": save}
+        , mode: "yaml"
+      });
+    }
+  }
 
 }, 500);
 
