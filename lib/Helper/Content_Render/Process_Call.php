345c166470a00b472de13109ef2e85b717527625
Andy Truong <andy@toila.net>
2014-03-06 07:18:21 +0700

    helper.content_render: Move before/after calls to an other class.

---
+++ lib/Helper/Content_Render/Process_Call.php
@@ -0,0 +1,45 @@
+<?php
+namespace Drupal\at_base\Helper\Content_Render;
+
+class Process_Call {
+  private $before;
+  private $after;
+
+  public function __construct($before, $after) {
+    $this->before = $before;
+    $this->after = $after;
+  }
+
+  public function callBefore($key = 'before') {
+    if (!empty($this->$key)) {
+      $this->runCallbacks($this->$key);
+    }
+  }
+
+  public function callAfter() {
+    $this->runBefore('after');
+  }
+
+  private function runCallbacks($calls) {
+    $cr = at_container('helper.controller.resolver');
+    foreach ($calls as $call) {
+      $call = is_string($call) ? array($call, array()) : $call;
+      if ($controller = $cr->get($call[0])) {
+        $args = isset($call[1]) ? $call[1] : array();
+        $this->prepareArguments($args);
+        call_user_func_array($controller, $args);
+      }
+    }
+  }
+
+  private function prepareArguments(&$args) {
+    foreach ($args as &$arg) {
+      if (is_array($arg)) {
+        $this->prepareArguments($arg);
+      }
+      elseif (is_string($arg) && preg_match('/^[@%].+/', $arg)) {
+        $arg = at_container('helper.real_path')->get($arg);
+      }
+    }
+  }
+}
