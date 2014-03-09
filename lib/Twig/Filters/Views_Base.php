c517147ba08c5c34d97ce4b65e150cd14442158b
Andy Truong <andy@toila.net>
2014-03-09 11:22:45 +0700

    Clean up \Drupal\at_base\Twig\Filters\Views

---
+++ lib/Twig/Filters/Views_Base.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Drupal\at_base\Twig\Filters;
+
+/**
+ * Callback for drupalView Twig filter.
+ *
+ */
+abstract class Views_Base {
+  protected $name;
+  protected $display_id;
+  protected $view;
+  protected $arguments = array();
+  protected $template;
+
+  public function constructTradition($name, $display_id = 'default', $arguments = array()) {
+    $this->setName($name);
+    $this->name = $name;
+    $this->setDisplayId($display_id);
+    $this->setArguments($arguments);
+  }
+
+  public function constructFancy($name, $options) {
+    $this->setName($name);
+
+    $loop = array(
+      'display_id' => 'default',
+      'arguments'  => NULL,
+      'template'   => NULL,
+      'pager'      => NULL,
+    );
+
+    foreach ($loop as $k => $default_value) {
+      $method = 'set' . str_replace(' ', '', ucwords(str_replace('_', ' ', $k)));
+      $value = isset($options[$k]) ? $options[$k] : $default_value;
+      if (!is_null($value)) {
+        $this->{$method}($value);
+      }
+    }
+  }
+
+  public function setName($name) {
+    if (!$this->view = views_get_view($name)) {
+      throw new \Exception('<!-- View not found: '. $this->name . ' -->');
+    }
+  }
+
+  public function setDisplayId($display_id) {
+    $this->display_id = $display_id;
+    $this->view->set_display($display_id);
+
+    if (!$this->view->access($this->display_id)) {
+      throw new \Exception('<!-- Access denied: '. $this->name .':'. $this->display_id .' -->');
+    }
+  }
+
+  public function setArguments($arguments) {
+    $this->arguments = $arguments;
+
+    if (is_array($this->arguments)) {
+      $this->view->set_arguments($this->arguments);
+    }
+  }
+
+  public function setTemplate($template) {
+    $this->template = at_container('helper.real_path')->get($template);
+  }
+
+  public function setPager($pager) {
+    if (!empty($pager)) {
+      $this->view->display[$this->display_id]->display_options['pager'] = $pager;
+    }
+  }
+}
