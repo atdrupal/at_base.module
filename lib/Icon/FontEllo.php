d0ed6352b70c57baa2faf7cebe8751f1453540ca
Tien Vo Xuan <tien@go1.com.au>
2014-03-12 15:09:21 +0700

    Load the right font.

--- lib/Icon/FontEllo.php
+++ lib/Icon/FontEllo.php
@@ -23,10 +23,21 @@ class FontEllo implements IconInterface {
 
         foreach ($config['glyphs'] as $glyph) {
           if ($glyph['css'] == $css_code) {
+
+            // Cache font path.
+            at_cache("atfont:font_path:{$css_code}, + 1 year", function() use ($config_file) {
+              global $base_root;
+              return $base_root . '/' . str_replace('config.yml', '', $config_file) . 'font';
+            });
+
+            // Cache font name.
+            at_cache("atfont:font_name:{$css_code}, + 1 year", function() use ($config) {
+              return $config['font']['fontname'];
+            });
+
             $code = $glyph['code'];
 
-            // Update code map.
-            return dechex($code);
+            return '\\' . dechex($code);
           }
         }
       }
@@ -34,11 +45,22 @@ class FontEllo implements IconInterface {
       return NULL;
     });
 
+    // Load font path and font name cache base on css code.
+    $cache = at_container('wrapper.cache')->get("atfont:font_path:{$css_code}", 'cache');
+    $font_path = $cache->data;
+    $cache = at_container('wrapper.cache')->get("atfont:font_name:{$css_code}", 'cache');
+    $font_name = $cache->data;
+    if (empty($font_path) || empty($font_name)) {
+      return '';
+    }
+
+    $this->addFontCss($font_path, $font_name);
+
     // Add css.
     if (empty($css_added[$css_code]) && !empty($code)) {
 
       drupal_add_css(
-        ".icon-$css_code:before { content: '$code'; }",
+        ".icon-$font_name-$css_code:before { content: '$code'; }",
         array(
           'type' => 'inline',
         )
@@ -48,12 +70,81 @@ class FontEllo implements IconInterface {
 
     // Add library.
     if (!$libraries_added) {
-      drupal_add_css(at_library('fontello', NULL, FALSE) . 'assets/icons/src/css/icons.css');
       drupal_add_css(at_library('fontello', NULL, FALSE) . 'assets/icons/src/css/animation.css');
       drupal_add_css(at_library('fontello', NULL, FALSE) . 'assets/icons/src/css/icons-ie7.css', array('browsers' => array('IE' => 'IE 7', '!IE' => FALSE)));
       $libraries_added = TRUE;
     }
 
-    return '<i class="icon-' . $css_code . '"></i>';
+    return '<i class="icon-' . $font_name . '-' . $css_code . '"></i>';
+  }
+
+  /**
+   * Add inline css code that load the right font base on font name.
+   *
+   * @staticvar boolean $font_added
+   * @param type $font_path
+   * @param type $font_name
+   */
+  public function addFontCss($font_path, $font_name) {
+    static $font_added = FALSE;
+
+    if (empty($font_added[$font_name])) {
+
+      drupal_add_css("@font-face {
+          font-family: '{$font_name}';
+          src: url('{$font_path}/{$font_name}.eot');
+          src: url('{$font_path}/{$font_name}.eot') format('embedded-opentype'),
+               url('{$font_path}/{$font_name}.woff') format('woff'),
+               url('{$font_path}/{$font_name}.ttf') format('truetype'),
+               url('{$font_path}/{$font_name}.svg') format('svg');
+          font-weight: normal;
+          font-style: normal;
+        }
+        /* Chrome hack: SVG is rendered more smooth in Windozze. 100% magic, uncomment if you need it. */
+        /* Note, that will break hinting! In other OS-es font will be not as sharp as it could be */
+        /*
+        @media screen and (-webkit-min-device-pixel-ratio:0) {
+          @font-face {
+            font-family: 'zocial';
+            src: url('{$font_path}/{$font_name}.svg') format('svg');
+          }
+        }
+        */
+
+         [class^=\"icon-{$font_name}-\"]:before, [class*=\" icon-{$font_name}-\"]:before {
+          font-family: \"{$font_name}\";
+          font-style: normal;
+          font-weight: normal;
+          speak: none;
+
+          display: inline-block;
+          text-decoration: inherit;
+          width: 1em;
+          margin-right: .2em;
+          text-align: center;
+          /* opacity: .8; */
+
+          /* For safety - reset parent styles, that can break glyph codes*/
+          font-variant: normal;
+          text-transform: none;
+
+          /* fix buttons height, for twitter bootstrap */
+          line-height: 1em;
+
+          /* Animation center compensation - magrins should be symmetric */
+          /* remove if not needed */
+          margin-left: .2em;
+
+          /* you can be more comfortable with increased icons size */
+          /* font-size: 120%; */
+
+          /* Uncomment for 3D effect */
+          /* text-shadow: 1px 1px 1px rgba(127, 127, 127, 0.3); */
+        }",
+        array('type' => 'inline')
+      );
+
+      $font_added[$font_name] = TRUE;
+    }
   }
 }
