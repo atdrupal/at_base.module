c1bf1c846255a96bc000f3e0092677a99806046f
Tien Vo Xuan <tien@go1.com.au>
2014-03-17 13:40:21 +0700

    Check fontello directory is a git repo.

--- lib/Drush/Command/FontEllo.php
+++ lib/Drush/Command/FontEllo.php
@@ -7,9 +7,12 @@ class FontEllo {
   public function execute() {
     $fontello_library_path = at_library('fontello', NULL, FALSE);
     $drupal_root = drush_get_context('DRUSH_DRUPAL_ROOT');
-//    $site_root = drush_get_context('DRUSH_DRUPAL_SITE_ROOT');
 
-    drush_shell_exec_interactive('cd ' . $drupal_root . '/' . $fontello_library_path. '&& cd .. && rm -rf fontello && git clone https://github.com/fontello/fontello.git');
+    $output = drush_shell_cd_and_exec($drupal_root . '/', 'git status');
+    if (empty($output)) {
+      throw new \Exception(printf("%s is not a git repo\n", $drupal_root . '/' . $fontello_library_path));
+    }
+
     drush_shell_exec_interactive('cd ' . $drupal_root . '/' . $fontello_library_path . '&& git submodule init');
     drush_shell_exec_interactive('cd ' . $drupal_root . '/' . $fontello_library_path . '&& git submodule update');
   }
