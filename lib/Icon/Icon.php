8496213fc9adbc9458408f732cbb3e4d1dc90146
Andy Truong <andy@toila.net>
2014-03-31 13:12:20 +1000

    + Introduce at_icon(), support font-awesome by default.

---
+++ lib/Icon/Icon.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Drupal\at_base\Icon;
+
+class Icon {
+  protected $tag;
+  protected $class;
+  protected $text;
+
+  public function __construct($class = '', $tag = 'i', $text = '') {
+    $this->class = $class;
+    $this->tag = $tag;
+    $this->text = $text;
+  }
+
+  public function render() {
+    return "<{$this->tag} class=\"{$this->class}\">{$this->text}</$this->tag>";
+  }
+
+}
