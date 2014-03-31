7ab6d7d8026babf3f1786874dec47c8c4037c927
Andy Truong <andy@toila.net>
2014-03-31 13:24:31 +1000

    Fix #188 by Tien Vo: Introduce at_icon(), support font-awesome by default.

---
+++ lib/Drupal/at_base/Tests/Unit/FontAwesomeTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\at_base\Tests\Unit;
+
+use Drupal\at_base\Helper\Test\UnitTestCase;
+
+/**
+ * drush test-run --dirty 'Drupal\at_base\Tests\Unit\FontAwesomeTest'
+ */
+class FontAwesomeTest extends UnitTestCase {
+  public function getInfo() {
+    return array('name' => 'AT Unit: Icon Fontawesome') + parent::getInfo();
+  }
+
+  public function testIconRendering() {
+    $css_code = 'fa-camera-retro';
+
+    \at_fake::drupal_add_css(function($data = NULL, $options = NULL) {
+      static $included = array();
+
+      if (!is_null($data)) {
+        $included[$data] = TRUE;
+      }
+
+      return $included;
+    });
+
+    $expected = '<i class="fa '. $css_code .'"></i>';
+    $actual = at_icon($css_code, 'icon.fontawesome');
+    $included_css = \at_fn::drupal_add_css();
+
+    $this->assertEqual($expected, $actual, 'at_icon() returns the the correct html for icon.');
+    $this->assertTrue(isset($included_css[at_library('fontawesome', NULL, FALSE) . 'css/font-awesome.css']), "fontawesome's css is included to page.");
+  }
+}
