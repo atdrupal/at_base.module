974807e20ce724c3e64f86f5f3b3fef529bee3c3
Andy Truong <andy@toila.net>
2014-03-31 13:19:50 +1000

    + Introduce at_icon(), support font-awesome by default
    continue: Add missing files.

---
+++ lib/Icon/FontAwesome.php
@@ -0,0 +1,21 @@
+<?php
+namespace Drupal\at_base\Icon;
+
+class FontAwesome implements IconInterface {
+  public function __construct() {
+    \at_fn::drupal_add_css(at_library('fontawesome', NULL, FALSE) . 'css/font-awesome.css');
+  }
+
+  /**
+   * Get Icon instance with information to generate icon tag.
+   *
+   * @param type $name
+   *   The name of icon in fontawesome.
+   *   Browse available icons at http://fortawesome.github.io/Font-Awesome/icons/
+   * @return \Drupal\at_base\Icon\Icon
+   *   Contain enough information to generate icon tag.
+   */
+  public function get($name) {
+    return new Icon($class = "fa {$name}");
+  }
+}
