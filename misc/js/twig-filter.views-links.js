0580faee815fecca57ef16970e5427944be129b7
Andy Truong <andy@toila.net>
2014-04-01 13:41:26 +1000

    Add (fake) contextual links for drupalView results.

--- misc/js/twig-filter.views-links.js
+++ misc/js/twig-filter.views-links.js
@@ -4,7 +4,11 @@ Drupal.behaviors.atbaseTwigFilterViews = {
   attach: function(context, settings) {
     $('.ctools-dropdown').once('atbaseTwigFilterViews')
     .parent()
-      .find('.ctools-dropdown').hide().end()
+      .addClass('atbase-views-wrapper')
+      .css({ display: 'block', position: 'relative' })
+      .find('.ctools-dropdown')
+        .css({ display: 'block', position: 'absolute', top: 10, right: 10 })
+        .hide().end()
     .hover(
       function() { $(this).find('.ctools-dropdown').show(); },
       function() { $(this).find('.ctools-dropdown').hide(); }
