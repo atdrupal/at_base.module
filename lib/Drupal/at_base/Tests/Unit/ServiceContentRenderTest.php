46112cf19f15347c9c99ffaa4159410d2f5f983c
Duythien Fedora20 <fcduythien@gmail.com>
2014-04-10 00:26:09 +0700

    Config: Support include @include

--- lib/Drupal/at_base/Tests/Unit/ServiceContentRenderTest.php
+++ lib/Drupal/at_base/Tests/Unit/ServiceContentRenderTest.php
@@ -90,4 +90,35 @@ class ServiceContentRenderTest extends UnitTestCase {
     $output = strip_tags($this->render->render($data),'<div>');
     $this->assertEqual('<div id="abc">Drupal</div>', $output);
   }
+  /**
+   * Render content with class
+   * @see  https://github.com/atdrupal/at_base/wiki/7.x-2.x-helper-content-render#14-render-content-with-class
+   */
+  public function testRenderClass(){
+    $expected = 'Demo render content with class Drupal';
+    $data = array();
+
+    $data['controller'] = array('At_Base_Test_Class', 'bar');
+    $data['arguments']  = array('name' => 'Drupal');
+
+    $this->assertEqual($expected, $this->render->render($data));
+
+  }
+  /**
+   * Cache rendered-output
+   */
+  public function testRenderCache(){
+
+    $data = array();
+    $data['template_string'] = '{{ view_name|lower }}';
+    $data['variables']['view_name'] = 'Hello Andy Truong';
+    
+    $data['variables']['cache'] = array(
+      'id' => 'products:latest:front',
+      'ttl' => '+ 30 minutes',
+      'tags' => ['node', 'products', 'home']
+    );
+    $output = $this->render->render($data);
+    $this->assertEqual('hello andy truong', $output);
+  }
 }
