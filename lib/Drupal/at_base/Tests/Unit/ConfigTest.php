761db34a3d5d938f500cff52a66428385e6b6986
Duythien Fedora20 <fcduythien@gmail.com>
2014-04-10 08:56:08 +0700

    Add case unit test Support include

--- lib/Drupal/at_base/Tests/Unit/ConfigTest.php
+++ lib/Drupal/at_base/Tests/Unit/ConfigTest.php
@@ -109,4 +109,29 @@ class ConfigTest extends UnitTestCase {
     $actual = at_id(new ModulesFetcher('at_base', $config_file = 'un_real_config_file'))->fetch($enabled_modules);
     $this->assertEqual($expected, $actual);
   }
+
+
+
+  /**
+   * Test case for at_config() function.
+   */
+  public function testConfigExternal() {
+
+    // Test simple value getting
+    $routes = at_config('atest_config')->getConfigExternal('routes');
+    $math =  array('name' => 'duythien');
+    $this->assertEqual($routes,$math);
+
+    // Test not found exception
+    try {
+      at_config('atest_config')->get('not_there');
+      $this->assertTrue('No exception thrown');
+    }
+    catch (\Drupal\at_base\Config\NotFoundException $e) {
+      $this->assertTrue('Throw NotFoundException if config item is not configured.');
+    }
+
+ 
+  }
+
 }
