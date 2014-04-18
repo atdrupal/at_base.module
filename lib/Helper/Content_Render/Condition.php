c8925efe92abf36c9ccffadf10c50f862ad56361
tien.xuan.vo <tien.xuan.vo@gmail.com>
2014-04-18 10:33:23 +0700

    Compose methods.

--- lib/Helper/Content_Render/Condition.php
+++ lib/Helper/Content_Render/Condition.php
@@ -121,48 +121,65 @@ class Condition {
     return $this->result;
   }
 
-  private function callCallback($callback) {
-    if (is_array($callback) && !empty($callback) && count($callback) <= 3 || is_string($callback)) {
-      if (is_string($callback)) {
-        $callable = $callback;
-        $arguments = array();
-      }
-      else {
-        // Use system argument flag.
-        if (count($callback) == 3) {
-          list($callable, $arguments, $has_system_arguments) = $callback;
-          if ($has_system_arguments) {
-            // Convert arguments to system arguments.
-            foreach ($arguments as &$argument) {
-              if (isset($this->args['build'][$argument])) {
-                // $argument is '#entity', '#entity_type', '#bundle', '#view_mode', '#language'...
-                // See more at Drupal\at_base\Hook\Entity\View_Alter::build().
-                $argument = $this->args['build'][$argument];
-              }
-            }
+  private function prepareStringCallback($callback, &$callable, &$arguments) {
+    $callable = $callback;
+    $arguments = array();
+  }
+
+  private function prepareArrayCallback($callback, &$callable, &$arguments) {
+    if (empty($callback)) {
+      return;
+    }
+
+    if (count($callback) == 3) {
+      // Use system argument flag.
+      list($callable, $arguments, $has_system_arguments) = $callback;
+      if ($has_system_arguments) {
+        // Convert arguments to system arguments.
+        foreach ($arguments as &$argument) {
+          if (isset($this->args['build'][$argument])) {
+            // $argument is '#entity', '#entity_type', '#bundle', '#view_mode', '#language'...
+            // See more at Drupal\at_base\Hook\Entity\View_Alter::build().
+            $argument = $this->args['build'][$argument];
           }
         }
-        else if (count($callback) == 2) {
-          list($callable, $arguments) = $callback;
-        }
-        else {
-          // Sure, you can pass array with only one element, we can convert it to string.
-          $callable = (string)reset($callback);
-          $arguments = array();
-        }
       }
+    }
+    else if (count($callback) == 2) {
+      list($callable, $arguments) = $callback;
+    }
+    else {
+      // Sure, you can pass array with only one element, we can convert it to string.
+      $callable = (string)reset($callback);
+      $arguments = array();
+    }
+  }
 
-      if (strpos($callable, '@' === 0)) {
-        // Getting service.
-        $callable = str_replace('@', '', $callable);
-        list($service_name, $method) = explode($callable, ':');
-        $service = at_container($service_name);
-        $callable = array($service, $method);
-      }
+  private function prepareServiceCallable(&$callable) {
+    if (strpos($callable, '@' === 0)) {
+      // Getting service.
+      $callable = str_replace('@', '', $callable);
+      list($service_name, $method) = explode($callable, ':');
+      $service = at_container($service_name);
+      $callable = array($service, $method);
+    }
+  }
 
-      if (is_callable($callable)) {
-        return call_user_func_array($callable, $arguments);
-      }
+  private function callCallback($callback) {
+    $callable = '';
+    $arguments = array();
+
+    if (is_string($callback)) {
+      $this->prepareStringCallback($callback, $callable, $arguments);
+    }
+    else if (is_array($callback)) {
+      $this->prepareArrayCallback($callback, $callable, $arguments);
+    }
+
+    $this->prepareServiceCallable($callable);
+
+    if (is_callable($callable)) {
+      return call_user_func_array($callable, $arguments);
     }
 
     throw new \Exception('Callback is not callable.');
