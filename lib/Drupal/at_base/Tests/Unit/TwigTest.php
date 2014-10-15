2ccd2e0d26284afd3163cdae34997437d7d4adf3
Andy Truong <thehongtt@gmail.com>
2014-10-15 14:46:36 +0700

    I am sad Travis.

--- lib/Drupal/at_base/Tests/Unit/TwigTest.php
+++ lib/Drupal/at_base/Tests/Unit/TwigTest.php
@@ -35,11 +35,11 @@ class TwigTest extends UnitTestCase {
     $twig = at_container('twig_string');
 
     // Use trim() function
-    $this->assertEqual($twig->render("{{  '  Drupal 7  '|fn__trim  }}"),  'Drupal 7');
+    $this->assertEqual($twig->render("{{  '  Drupal 7  '|fn__trim  }}"), 'Drupal 7');
     $this->assertEqual($twig->render("{{  fn__trim('  Drupal 7  ')  }}"), 'Drupal 7');
 
     // Use At_Base_Test_Class::helloStatic()
-    $this->assertEqual($twig->render("{{  'Drupal 8'|At_Base_Test_Class__class__helloStatic  }}"),  'Hello Drupal 8');
+    $this->assertEqual($twig->render("{{  'Drupal 8'|At_Base_Test_Class__class__helloStatic  }}"), 'Hello Drupal 8');
     $this->assertEqual($twig->render("{{  At_Base_Test_Class__class__helloStatic('Drupal 8')  }}"), 'Hello Drupal 8');
 
     // Use At_Base_Test_Class::helloProperty()
@@ -47,8 +47,8 @@ class TwigTest extends UnitTestCase {
     $this->assertEqual($twig->render("{{  At_Base_Test_Class__obj__helloProperty('PHP')  }}"), 'Hello PHP');
 
     // Namespace
-    $this->assertEqual($twig->render("{{  'Namespace'|ns_Drupal__atest_base__Service_1__class__helloStatic  }}"),  'Hello Namespace');
-    $this->assertEqual($twig->render("{{  ns_Drupal__atest_base__Service_1__class__helloStatic('Namespace')  }}"), 'Hello Namespace');
+    $this->assertEqual($twig->render("{{  'Namespace'|ns_Drupal__atest_base__Service1__class__helloStatic  }}"), 'Hello Namespace');
+    $this->assertEqual($twig->render("{{  ns_Drupal__atest_base__Service1__class__helloStatic('Namespace')  }}"), 'Hello Namespace');
   }
 
   public function testTwigStringLoader() {
@@ -59,13 +59,13 @@ class TwigTest extends UnitTestCase {
   public function testCacheFilter() {
     $twig = at_container('twig_string');
 
-    $string_1  = "{% set options = { cache_id: 'atestTwigCache:1' } %}";
+    $string_1 = "{% set options = { cache_id: 'atestTwigCache:1' } %}";
     $string_1 .= "\n {{ 'atest_base.service_1:hello' | cache(options) }}";
-    $string_2  = "{% set options = { cache_id: 'atestTwigCache:2' } %}";
+    $string_2 = "{% set options = { cache_id: 'atestTwigCache:2' } %}";
     $string_2 .= "\n {{ 'At_Base_Test_Class::helloStatic' | cache(options) }}";
-    $string_3  = "{% set options = { cache_id: 'atestTwigCache:3' } %}";
+    $string_3 = "{% set options = { cache_id: 'atestTwigCache:3' } %}";
     $string_3 .= "\n {{ 'atest_base_hello' | cache(options) }}";
-    $string_4  = "{% set options  = { cache_id: 'atestTwigCache:4' } %}";
+    $string_4 = "{% set options  = { cache_id: 'atestTwigCache:4' } %}";
     $string_4 .= "\n {% set callback = { callback: 'atest_base_hello', arguments: ['Andy Truong'] } %}";
     $string_4 .= "\n {{ callback | cache(options) }}";
     for ($i = 1; $i <= 4; $i++) {
