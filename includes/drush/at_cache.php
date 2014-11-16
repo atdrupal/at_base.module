a6b1f480ff39dc7855648940f50bea0ccb7633a9
Andy Truong <thehongtt@gmail.com>
2014-11-16 16:29:27 +0700

    Remove at_cache_flush_by_tags().

--- includes/drush/at_cache.php
+++ includes/drush/at_cache.php
@@ -1,6 +1,7 @@
 <?php
 
-function drush_at_cache_clear() {
-  $tags = func_get_args();
-  at_cache_flush_by_tags($tags);
+function drush_at_cache_clear()
+{
+    $tags = func_get_args();
+    return at()->getApi()->getCacheAPI()->getTagFlusher()->flush($tags);
 }
