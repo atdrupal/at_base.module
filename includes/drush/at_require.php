70ab64233a1361c1f7a03eed584f80c904531aae
Andy Truong <thehongtt@gmail.com>
2014-11-16 16:46:26 +0700

    No Drush 4.

--- includes/drush/at_require.php
+++ includes/drush/at_require.php
@@ -20,22 +20,20 @@ class DrushMakeProject_AtRequire_Library extends \DrushMakeProject_Library
 }
 
 /**
+ * Implements drush_hook_COMMAND_pre_validate()
+ */
+function drush_at_require_pm_enable_pre_validate($module)
+{
+    // Module was already processed, no need redo
+    if (!module_exists($module)) {
+        drush_at_require($module);
+    }
+}
+
+/**
  * Callback for at_require command.
  */
 function drush_at_require($module = 'all')
 {
     (new AtRequireCommand($module))->execute();
-}
-
-/**
- * Implements drush_hook_COMMAND_pre_validate()
- */
-function drush_at_require_pm_enable_pre_validate($module)
-{
-    _drush_at_check_yml_reader();
-
-    // Module was already processed, no need redo
-    if (!module_exists($module)) {
-        drush_at_require($module);
-    }
 }
